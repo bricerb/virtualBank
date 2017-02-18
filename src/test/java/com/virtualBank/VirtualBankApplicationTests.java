9724d24ac3578cc380a6b07614e355b688434731
Richard Blanch <bricerb@gmail.com>
2017-02-17 21:44:37 -0500

    Initial commit

---
+++ src/test/java/com/virtualBank/VirtualBankApplicationTests.java
@@ -0,0 +1,16 @@
+package com.virtualBank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VirtualBankApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
